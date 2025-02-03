d701e1bd5b5c0f2b39343bd62a0aff980a09c39b
Thanushangit <sinnarasathanushan2002@gmail.com>
2025-02-03 19:19:25 +0530

    change

--- Practises/practise 03/login-page-psw-match/src/App.jsx
+++ Practises/practise 03/login-page-psw-match/src/App.jsx
@@ -1,8 +1,23 @@
 
+import { useState } from 'react'
 import './App.css'
 
 function App() {
   
+  const [psw1,setPsw1]=useState();
+  const [psw2,setpsw2]=useState();
+  const[re,setRe]=useState(false)
+
+  async function Handler(e){
+      await setpsw2(e.target.value);
+      await if(psw1==psw2){
+        setRe(false)
+      }
+
+      else{
+        setRe(true)
+      }
+  }
 
   return (
     <>
@@ -14,12 +29,12 @@ function App() {
   </div>
   <div className="mb-3">
     <label  className="form-label">Password</label>
-    <input type="password" className="form-control" />
+    <input type="password" value={psw1} onChange={(e)=>setPsw1(e.target.value)} className="form-control" />
   </div>
   <div className="mb-3">
     <label  className="form-label">Re-Enter Password</label>
-    <input type="password" className="form-control" />
-    <label  className="form-label text-danger">Password Doesn't match</label>
+    <input type="password" value={psw2} onChange={Handler} className="form-control" />
+   {re && <label  className="form-label text-danger">Password Doesn't match</label>}
   </div>
   <div className="mb-3 form-check">
     <input type="checkbox" className="form-check-input" />
