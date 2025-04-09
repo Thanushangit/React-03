408af8979f130014f9ece2e1300bad436a160e9a
Thanushangit <sinnarasathanushan2002@gmail.com>
2025-04-09 11:25:41 +0530

    changed

--- projects/Filter_items/filter/src/App.jsx
+++ projects/Filter_items/filter/src/App.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import './App.css'
+import Navbar from './Components/Navbar'
+import SideNavbar from './Components/SideNavbar'
+import Contents from './Components/Contents'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+     <Navbar/>
+
+      <div className="content gap-5">
+        <SideNavbar/>
+        <Contents/>
+      </div>
+    </div>
   )
 }
 
